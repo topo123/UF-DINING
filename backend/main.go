475eacd16258e1420b64ba1b304fd36ede63e0bd
Hugo Liu <98724522+hugoliu-code@users.noreply.github.com>
2025-10-27 14:49:20 -0400

    Basic mongo ping

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,39 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"net/http"
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	"go.mongodb.org/mongo-driver/v2/mongo/readpref"
 )
 
 func main() {
+	// Use the SetServerAPIOptions() method to set the version of the Stable API on the client
+
+	godotenv.Load(".env")
+
+	user_string := os.Getenv("MONGO_USER_PASSWORD")
+
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+
+	opts := options.Client().ApplyURI(user_string).SetServerAPIOptions(serverAPI)
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(opts)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+	// Send a ping to confirm a successful connection
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		panic(err)
+	}
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
 }
