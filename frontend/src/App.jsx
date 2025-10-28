450fd0972ca907bc4ba5a467d07e4d03238ce44b
Om Vaddi <om.r.vaddi@gmail.com>
2025-10-27 23:28:30 -0400

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='title'>
+        Dinr
+      </h1>
+    </>
+  )
+}
+
+export default App;
